3da6e5c4e329d696f0dbe1b3eab40a2ff491566f
David Liao <liao.canada@gmail.com>
2020-03-02 21:33:00 -0500

    Use getInitialProps for initializing props

--- pages/products.js
+++ pages/products.js
@@ -1,24 +1,38 @@
-import { useRouter } from 'next/router';
-
 import Layout from "../components/Layout";
 import Product from "../components/products/Product";
 
-export default function Products() {
-	const router = useRouter();
-
-	const dummyProducts = [
-		{id: "pid001", name: "product1", price: 1.00}, 
-		{id: "pid002", name: "product2", price: 2.00}, 
-		{id: "pid003", name: "product3", price: 3.00},
-	];
-
+const Products = props => {
+	const { dummyProducts, filter } = props;
+	console.log(filter);
+	
 	return (
 		<Layout>
 			<h1>All Products</h1>
-			{router.query.name &&
-				<p>There was a name filter for {router.query.name} but this is not supported yet.</p>
+			{filter.name &&
+				<p>There was a name filter for {filter.name} but this is not supported yet.</p>
 			}
 			{dummyProducts.map(({id, name, price}) => <Product key={id} id={id} name={name} price={price} />)}
 		</Layout>
 	);
-}
+};
+
+Products.getInitialProps = async context => {
+	const { name, id, minPrice, maxPrice } = context.query;
+
+	// TEST: page doesn't load until props are retrieved
+	await new Promise(r => setTimeout(r, 5000));
+
+	// TODO get info about all products that fit the filter from DB
+	return {
+		dummyProducts: [
+			{id: "pid001", name: "product1", price: 1.00}, 
+			{id: "pid002", name: "product2", price: 2.00}, 
+			{id: "pid003", name: "product3", price: 3.00},
+		],
+		filter: {
+			name, id, minPrice, maxPrice
+		}
+	};
+};
+
+export default Products;
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,13 +1,25 @@
 import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
 
-export default function Post() {
-  const router = useRouter();
+const Product = props => {
+	const { isbn, name } = props;	
 
   return (
     <Layout>
-      <h1>Some Product</h1>
-      <p>ID: {router.query.id}</p>
+      <h1>{name}</h1>
+      <p>ID: {isbn}</p>
     </Layout>
   );
-}
+};
+
+Product.getInitialProps = async context => {
+	const { id } = context.query;
+	// TODO get info about product with ID from DB
+
+	return {
+		isbn: id,
+		name: "Product with name " + id
+	};
+};
+
+export default Product;
