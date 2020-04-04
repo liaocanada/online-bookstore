86b8c0d7525eb9f60e0dac9e82ccbb4a5078b936
David Liao <liao.canada@gmail.com>
2020-04-03 21:01:55 -0400

    Turn isbn to VARCHAR since it may contain the letter X

--- scripts/sqlGenerators/bookGenerator.js
+++ scripts/sqlGenerators/bookGenerator.js
@@ -9,7 +9,7 @@ const createTable = "CREATE TABLE books(" +
         "price NUMERIC(8, 2)," + 
         "publisher_price NUMERIC(8, 2)," + 
         "sold_count INT," + 
-        "isbn INT," + 
+        "isbn VARCHAR(9)," + 
         "series VARCHAR(255)," + 
         "format VARCHAR(255)," + 
         "pages INT" + 
@@ -18,30 +18,20 @@ console.log(createTable);
 
 const createSqlTransformStream = () => {
     const mapToSql = (book, encoding, callback) => {
-        const inject = {
-            publication_year: book.original_publication_year,
-            average_rating: book.average_rating,
-            ratings_count: book.ratings_count,
-            isbn: book.isbn
-        };
-        const hasNonNumber = Object.values(inject).find(val => typeof(val) !== "number");
-        if (hasNonNumber) {
-            throw new Error("Risk of SQL injection!");
-        }
-
         const format = "INSERT INTO books " + 
             "(name, description, price, publisher_price, sold_count, " + 
                 "isbn, series, format, pages) " + 
             "VALUES " +
-                `(%L, %L, 12, 11, 123213, ` + 
-                `${inject.isbn}, 'todo-series', 'random(paperback, hardcover, ebook)', 123);`;
+                `(%L, %L, 12, 11, 123213, %L, %L, %L, 123);`;
 
-        const bookSql = escape(
-            format,
+        const bookSql = escape(format,
             book.original_title, 
             // `\nDROP sampletable;'\nSELECT * FROM members; DROP members----A book \n  called ${book.title} published in ${book.original_publication_year} ` + 
             `A book called ${book.title} published in ${book.original_publication_year} ` + 
-                `and rated ${book.average_rating} (${book.ratings_count} reviews)`
+                `and rated ${book.average_rating} (${book.ratings_count} reviews)`,
+            book.isbn.toString(),  // ISBN might be number or string
+            "todo-series",
+            "random(paperback, hardcover, ebook)"
         );
 
         callback(null, bookSql);
