3af9d2b7944d86a025292418ded8783d7eef9ff9
David Liao <liao.canada@gmail.com>
2020-04-03 22:14:50 -0400

    Use faker module to generate dummy values

--- scripts/sqlGenerators/bookGenerator.js
+++ scripts/sqlGenerators/bookGenerator.js
@@ -1,6 +1,7 @@
 const papa = require("papaparse");
 const request = require("request");
 const escape = require("pg-escape");
+const faker = require("faker");
 
 const createTable = "CREATE TABLE books(" +
         "product_id SERIAL PRIMARY KEY REFERENCES products," +  // TODO is this valid?
@@ -16,22 +17,48 @@ const createTable = "CREATE TABLE books(" +
     ");";
 console.log(createTable);
 
+faker.seed(123);
+
 const createSqlTransformStream = () => {
     const mapToSql = (book, encoding, callback) => {
-        const format = "INSERT INTO books " + 
+        // Title is in the format: Bridget Jones's Diary (Bridget Jones, #1)
+        const getSeries = () => {
+            if (!book.title) return null;
+            const title = book.title.toString();
+            
+            if (title.includes("(") && 
+                title.includes(")") && 
+                title.includes("#") &&
+                title.includes(",")) {
+                    const startIndex = title.lastIndexOf("(") + 1;
+                    const endIndex = title.lastIndexOf(",");
+                    return title.substring(startIndex, endIndex);
+            }
+            else return null;
+        };
+
+        const price = parseFloat(faker.commerce.price(4.99, 29.99, 2));
+        const publisherPrice = parseFloat(faker.finance.amount(0.3*price, 0.8*price, 2));
+        const soldCount = faker.random.number({min: 0, max: 50000});
+        const series = getSeries();
+        const format = faker.random.arrayElement(["paperback", "paperback", "paperback", "hardcover", "ebook"]);
+        const pages = faker.random.number({min: 100, max: 500});
+
+        const sqlFormat = "INSERT INTO books " + 
             "(name, description, price, publisher_price, sold_count, " + 
                 "isbn, series, format, pages) " + 
             "VALUES " +
-                `(%L, %L, 12, 11, 123213, %L, %L, %L, 123);`;
+                `(%L, %L, ${price}, ${publisherPrice}, ${soldCount}, ` + 
+                `%L, %L, %L, ${pages});`;
 
-        const bookSql = escape(format,
+        const bookSql = escape(sqlFormat,
             book.original_title, 
             // `\nDROP sampletable;'\nSELECT * FROM members; DROP members----A book \n  called ${book.title} published in ${book.original_publication_year} ` + 
             `A book called ${book.title} published in ${book.original_publication_year} ` + 
                 `and rated ${book.average_rating} (${book.ratings_count} reviews)`,
             book.isbn.toString(),  // ISBN might be number or string
-            "todo-series",
-            "random(paperback, hardcover, ebook)"
+            series,
+            format,
         );
 
         callback(null, bookSql);
