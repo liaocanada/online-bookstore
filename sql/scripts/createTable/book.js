cb3d9c4585525accb2b35ef02346493860dba844
David Liao <liao.canada@gmail.com>
2020-04-09 16:04:23 -0400

    Change schema for book

--- sql/scripts/createTable/book.js
+++ sql/scripts/createTable/book.js
@@ -1,10 +1,10 @@
 module.exports = outputStream =>
     outputStream.write(
         "CREATE TABLE book(" +
-            "product_id SERIAL PRIMARY KEY REFERENCES products, " +  // TODO is this valid?
+            "product_id SERIAL PRIMARY KEY, " +
             "name VARCHAR(255), " + 
             "description VARCHAR(2047), " + 
-            "price NUMERIC(8, 2), " + 
+            "price NUMERIC(14, 2), " + 
             "publisher_price NUMERIC(8, 2), " + 
             "sold_count INT, " + 
             "isbn VARCHAR(9), " + 
