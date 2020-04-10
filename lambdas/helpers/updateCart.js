f7d936d649fb08520a8edaa24f88a4f20497ac2d
Eric Pham <EricPham@cmail.carleton.ca>
2020-04-10 13:32:03 -0400

    Update updateCart.js

--- lambdas/helpers/updateCart.js
+++ lambdas/helpers/updateCart.js
@@ -2,7 +2,7 @@
 // update the last_edited attribute for a user
 const updateCart = (client,user) => {
     const now = new Date();
-    const statement = "update cart set last_edited = $1 where username = '$2';";
+    const statement = "update cart set last_edited = $1 where username = $2;";
     const values = [now, user];
 
     const res = await client.query(statement, values);
