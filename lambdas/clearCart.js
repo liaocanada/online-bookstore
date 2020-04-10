466d4cb0ce9bbf0ca563bdb947168e5283539c66
Eric Pham <EricPham@cmail.carleton.ca>
2020-04-10 13:18:36 -0400

    Update clearCart.js

--- lambdas/clearCart.js
+++ lambdas/clearCart.js
@@ -5,14 +5,14 @@ exports.handler = async (event, context) => {
     const client = await connect();
 
     const user = "testUser";
-    const prod_id = "1";
 
     // update user's cart last_edited
     const update = require("./helpers/updateCart")(client,user);
 
     // clear all products for a user
-    const query = "delete from cart_product where username = '"+user+"';";
-    const res = await client.query(query);
+    const statement = "delete from cart_product where username = '$1';";
+    const values = [user];
+    const res = await client.query(statement, values);
 
     client.end();
 
