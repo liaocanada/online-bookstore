ec57e1ec8a10e57133400b7712ab8c28b15d7381
Eric Pham <EricPham@cmail.carleton.ca>
2020-04-10 13:16:22 -0400

    Update addProductToCart.js

--- lambdas/addProductToCart.js
+++ lambdas/addProductToCart.js
@@ -5,17 +5,17 @@ exports.handler = async (event, context) => {
     const client = await connect();
 
     const user = "testUser";
-    const prod_id = "1";
+    const prod_id = 1;
     const quantity = 1;
 
     // update user's cart last_edited
     const update = require("./helpers/updateCart")(client,user);
 
     // add product to cart_product
-    // TODO fix risk of SQL injection (remove this comment when fixed)
-    const query = "insert into cart_product (username, product_id, quantity) " + "values ('"+user+"', '"+prod_id+"', '"+quantity+"');";
+    const statement = "insert into cart_product (username, product_id, quantity) values ($1, $2, $3);";
+    const values = [user, prod_id, quantity];
 
-    const res = await client.query(query);
+    const res = await client.query(statement, values);
 
     client.end();
 
