9059b3b86a1e6314d165d44e69756177b3058b0d
Eric Pham <EricPham@cmail.carleton.ca>
2020-04-11 15:01:37 -0400

    Update getProductbyId

--- lambdas/getProductbyId.js
+++ lambdas/getProductbyId.js
@@ -4,15 +4,18 @@ const form200Response = require("./helpers/form200Response");
 exports.handler = async (event, context) => {
     const client = await connect();
 
-    const product_id = 1;
+    if (!isNaN(event.pathParameters.product_id)) {
+        const product_id = parseInt(event.pathParameters.product_id);
 
-    // get user info
-    const statement = "select distinct product_id,name,description,price,isbn,series,format,pages,string_agg(author_name, ', ') as authors,string_agg(genre, ', ') as genres from product natural full outer join ((book natural join writes) natural join book_genre) where product_id = $1 group by product_id,name,description,price,isbn,series,format,pages;";
-    const values = [product_id];
+        // get user info
+        const statement = "select distinct product_id,name,description,price,isbn,series,format,pages,string_agg(author_name, ', ') as authors,string_agg(genre, ', ') as genres from product natural full outer join ((book natural join writes) natural join book_genre) where product_id = $1 group by product_id,name,description,price,isbn,series,format,pages;";
+        const values = [product_id];
 
-    const res = await client.query(statement, values);
+        const res = await client.query(statement, values);
 
-    client.end();
+        client.end();
 
-    return form200Response(res.rows);
+        return form200Response(res.rows);
+    }
+    
 };
