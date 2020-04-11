9b65b1b01e899606fc60276377b28cd0771597d1
Eric Pham <EricPham@cmail.carleton.ca>
2020-04-11 10:20:54 -0400

    Add getAllProducts with filter functionality

--- lambdas/getAllProducts.js
+++ lambdas/getAllProducts.js
@@ -4,12 +4,34 @@ const form200Response = require("./helpers/form200Response");
 exports.handler = async (event, context) => {
     const client = await connect();
 
-    // select all products in user's cart
-    const statement = "select * from product natural full outer join book;";
+    filtered = true;
 
-    const res = await client.query(statement);
+    if (filtered) {
+        filter_by = 'isbn';
+        search = "345418263";
 
-    client.end();
+        // select all products in user's cart
+        const statement = "select * from product natural full outer join ((book natural join writes) natural join book_genre) where "+filter_by+" = $1;";
+        const values = [search];
 
-    return form200Response(res.rows);
+        const res = await client.query(statement, values);
+
+        client.end();
+
+        return form200Response(res.rows);
+
+    } else {
+
+        // select all products in user's cart
+        const statement = "select * from product natural full outer join ((book natural join writes) natural join book_genre);";
+
+        const res = await client.query(statement);
+
+        client.end();
+
+        return form200Response(res.rows);
+
+    }
+
+    
 };
