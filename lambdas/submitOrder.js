5ae9613010e76da965c538c2d01629076b3cf83f
Eric Pham <EricPham@cmail.carleton.ca>
2020-04-11 13:59:53 -0400

    Update submitOrder

--- lambdas/submitOrder.js
+++ lambdas/submitOrder.js
@@ -17,16 +17,37 @@ exports.handler = async (event, context) => {
 
     let res = await client.query(statement, values);
 
-    //console.log(res.rows[0].order_number); // to get order number
-    
+    const order_number = res.rows[0].order_number;
 
     // get all items from user cart cart_product and add them to order_product
+    // get items from cart first
+    statement = "select product_id, quantity, price from cart_product natural join product where username=$1";
+    values = [username];
+    res = await client.query(statement, values);
+    // add all to order_product
+    for (let i=0; i<res.rows.length; i++) {
+        statement = "insert into order_product (order_number, product_id, quantity, price) values ($1, $2, $3, $4)";
+        values = [order_number, res.rows[i].product_id, res.rows[i].quantity, res.rows[i].price];
+        res = await client.query(statement, values);
+    }
 
     // get all coupons from cart_coupon, but add to order_coupon
+    statement = "select coupon_code from cart_coupon where username=$1";
+    values = [username];
+    res = await client.query(statement, values);
+    // add all to order_product
+    for (let i=0; i<res.rows.length; i++) {
+        statement = "insert into order_coupon (order_number, coupon_code) values ($1, $2)";
+        values = [order_number, res.rows[i].coupon_code];
+        res = await client.query(statement, values);
+    }
 
-    // clear user cart (by calling clearCart.js)
+    // clear user cart
+    statement = "delete from cart_product where username = $1;";
+    values = [username];
+    res = await client.query(statement, values);
 
     client.end();
 
-    return form201Response({order_number: res.rows[0].order_number});
+    return form201Response({order_number: order_number});
 };
