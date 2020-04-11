805213f5811568037bd343c0562bfb78b34a847f
Eric Pham <EricPham@cmail.carleton.ca>
2020-04-11 17:13:44 -0400

    Add more params to lambda

--- lambdas/addProductToCart.js
+++ lambdas/addProductToCart.js
@@ -4,12 +4,12 @@ const form201Response = require("./helpers/form200Response");
 exports.handler = async (event, context) => {
     const client = await connect();
 
-    const user = "testUser";
+    const user = event.pathParameters.username;
     const prod_id = 1;
     const quantity = 1;
 
     // update user's cart last_edited
-    //const update = require("./helpers/updateCart")(client,user);
+    const update = require("./helpers/updateCart")(client,user);
 
     // add product to cart_product
     const statement = "insert into cart_product (username, product_id, quantity) values ($1, $2, $3);";
