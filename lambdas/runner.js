fb51333e6c35021896e7f6c63e35812cba258707
David Liao <liao.canada@gmail.com>
2020-04-11 12:15:09 -0400

    Modify lambda runner to allow events to be passed through the parameter

--- lambdas/runner.js
+++ lambdas/runner.js
@@ -1,6 +1,15 @@
 // Runs one of the lambdas contained in this folder locally.
 // (normally they are supposed to be run on AWS Lambda)
+
+// Usage:
+// node runner.js getAllProducts -p ./testEvents/sampleRequest.json
+const path = require("path");
+
+const dashPFlagIndex = process.argv.indexOf("-p");
+const parameterFile = (dashPFlagIndex === -1) ? undefined : process.argv[dashPFlagIndex + 1];
+const args = parameterFile ? require(path.join(__dirname, parameterFile)) : undefined;
+
 if (process.argv[2])
-    require(`./${process.argv[2]}`).handler().then(result => console.log(result));
+    require(`./${process.argv[2]}`).handler(args).then(result => console.log(result));
 else
-    require("./index.js").handler().then(result => console.log(result));
+    require("./index.js").handler(args).then(result => console.log(result));
