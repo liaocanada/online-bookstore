98b4441c947264c2ba46923e8eaf44a416def9bd
David Liao <liao.canada@gmail.com>
2020-04-12 12:48:20 -0400

    Bugfix getProductById lambda

--- lambdas/getProductbyId.js
+++ lambdas/getProductbyId.js
@@ -6,19 +6,19 @@ const formTextResponse = require("./helpers/formTextResponse");
 
 exports.handler = async (event, context) => {
 
-    if (!isInt(event.pathParameters.product_id)) {
-        return formTextResponse(404, "Invalid input");
+    // Verify product ID from path parameters
+    const product_id = parseInt(event.pathParameters.product_id);
+    if (!isInt(product_id)) {
+        return formTextResponse(404, product_id + " is not a valid product ID.");
     }
 
     const client = await connect();
 
-    // Product ID from path parameters
-    const product_id = parseInt(event.pathParameters.product_id);
 
     // get user info
     const statement = "select distinct product_id,name,description,price,isbn,series,format,pages,string_agg(author_name, ', ') as authors,string_agg(genre, ', ') as genres "+
-                    +"from product natural full outer join ((book natural join writes) natural join book_genre) "+
-                    +"where product_id = $1 group by product_id,name,description,price,isbn,series,format,pages;";
+        "from product natural full outer join ((book natural join writes) natural join book_genre) "+
+        "where product_id = $1 group by product_id,name,description,price,isbn,series,format,pages;";
     const values = [product_id];
 
     const res = await client.query(statement, values);
