4b314c0c65f8c35d59bb849d4d37336c9b24d58e
Eric Pham <EricPham@cmail.carleton.ca>
2020-04-12 18:54:01 -0400

    Change product frontend

--- pages/products.js
+++ pages/products.js
@@ -4,7 +4,78 @@ import config from "../config/config";
 import Layout from "../components/Layout";
 import Product from "../components/products/Product";
 import Search from "../components/products/Search";
+import Link from "next/link";
+import { Button } from '@material-ui/core';
+import React from 'react';
 
+class Products extends React.Component {
+	constructor(props) {
+	  super(props);
+	//   console.log(props.products);
+	//   console.log("poopoo");
+	  this.state = {value: '', products: props.products};
+	}
+
+	static async getInitialProps(context) {
+		console.log("getInitialProps", context);
+		let req = config.API_GATEWAY_ENDPOINT + "/products";
+		const filters = [];
+		Object.keys(context.query).forEach(key => {
+			if (context.query[key]) {
+				filters.push(key + "=" + context.query[key]);
+			}
+		});
+		// Join the parts into a query
+		const filtersString = (filters.length === 0) ? "" : ("?" + filters.join("&"));
+
+		const res = await fetch(req+filtersString);
+		console.log("fetch finished", res);
+		return {
+			products: await res.json()
+		};
+	};
+
+	/*async updateProducts(query) {
+		let req = config.API_GATEWAY_ENDPOINT + "/products";
+
+		const filters = [];
+		Object.keys(query).forEach(key => {
+			if (query[key]) {
+				filters.push(key + "=" + query[key]);
+			}
+		});
+		// Join the parts into a query
+		const filtersString = (filters.length === 0) ? "" : ("/?" + filters.join("&"));
+
+		const res = await fetch(req+filtersString);
+		console.log(res);
+		this.state.products = await res.json();
+
+	}*/
+  
+	handleChange(event) {
+	  this.setState({value: event.target.value});
+	}
+  
+	render() {
+			return (
+				<Layout>
+					<h1>Search Products</h1>
+						<p>{this.state.value}</p>
+						<label>
+							<input type="text" onChange={() => this.handleChange()} />
+						</label>
+						<Link href={"/products?"+this.state.value}>
+							<Button variant="contained">Search</Button>
+						</Link>
+					{this.state.products.map(({product_id, name, description, price, isbn, authors, genres}) => <Product key={name} id={product_id} name={name} description={description} price={parseFloat(price) }  isbn={isbn} authors={authors} genres={genres} />)}
+				</Layout>
+			  );
+		
+	}
+  }
+
+/*
 const Products = props => {
 	console.log(products);
 	const { products } = props;
@@ -19,13 +90,25 @@ const Products = props => {
 };
 
 Products.getInitialProps = async context => {
+	let req = config.API_GATEWAY_ENDPOINT + "/products";
 
-    const res = await fetch(config.API_GATEWAY_ENDPOINT + "/products");
+	const filters = [];
+    Object.keys(context.query).forEach(key => {
+        if (context.query[key]) {
+            filters.push(key + "=" + context.query[key]);
+        }
+    });
+    // Join the parts into a query
+    const filtersString = (filters.length === 0) ? "" : ("/?" + filters.join("&"));
+
+    const res = await fetch(req+filtersString);
 	console.log(res);
 	return {
 		products: await res.json()
 	};
-};
+};*/
+
+
 
 /*const Products = props => {
     const { products, filter } = props;
