5fca570e778c92afa8fc2feb1a985d30308341d6
David Liao <liao.canada@gmail.com>
2020-04-12 12:41:16 -0400

    Add NODE_PATH as environment variable when deploying

--- lambdas/deploy-env.bat
+++ lambdas/deploy-env.bat
@@ -6,4 +6,4 @@ set DATABASE="bookstore"
 set /p USERNAME=Enter database username:
 set /p PASSWORD=Enter database password:
 cls
-aws lambda update-function-configuration --function-name %toDeploy% --environment "Variables={DB_HOST=%HOST%, DB_DATABASE=%DATABASE%, DB_USER="%USERNAME%", DB_PASSWORD="%PASSWORD%"}"
+aws lambda update-function-configuration --function-name %toDeploy% --environment "Variables={DB_HOST=%HOST%, DB_DATABASE=%DATABASE%, DB_USER="%USERNAME%", DB_PASSWORD="%PASSWORD%", NODE_PATH=/opt/lambdas/node_modules}"
