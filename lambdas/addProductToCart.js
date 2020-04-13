4e3d0612dd104e3043eaa005f6dd3fcdb5383707
Eric Pham <EricPham@cmail.carleton.ca>
2020-04-13 18:59:01 -0400

    Update addProducttoCart

--- lambdas/addProductToCart.js
+++ lambdas/addProductToCart.js
@@ -3,17 +3,17 @@ const formJsonResponse = require("./helpers/formJsonResponse");
 
 exports.handler = async (event, context) => {
     const client = await connect();
+    const requestBody = JSON.parse(event.body);
+    const { product_id, quantity } = requestBody;
 
     const user = event.pathParameters.username;
-    const prod_id = 1;
-    const quantity = 1;
 
     // update user's cart last_edited
     const update = require("./helpers/updateCart")(client,user);
 
     // add product to cart_product
     const statement = "insert into cart_product (username, product_id, quantity) values ($1, $2, $3);";
-    const values = [user, prod_id, quantity];
+    const values = [user, product_id, quantity];
 
     const res = await client.query(statement, values);
 
