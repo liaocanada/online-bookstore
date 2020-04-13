708fa7245a2effb4a463a895fcce6b12a1d1b2fa
David Liao <liao.canada@gmail.com>
2020-04-12 20:18:51 -0400

    Make Search component

--- components/products/Search.js
+++ components/products/Search.js
@@ -1,10 +1,28 @@
-/*import Link from "next/link";
+import React from 'react';
+import { Button, TextField } from '@material-ui/core';
 
-const Product = ({id, name, price}) => {
-	//const {id, name, price} = props;
-	return (
+class Search extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = { text: "" };
+	}
+
+	render() {
+		const submit = this.props.onSubmit;
+		if (!submit) console.log("onsubmit is null!");
 		
-	);
+		return (
+			<>
+				<TextField id="standard-basic" 
+					label="Genre" 
+					value={this.state.text}
+					onChange={event => this.setState({ text: event.target.value })}
+				/>
+				<Button variant="contained" onClick={() => submit(this.state.text)}>Search</Button>
+			</>
+		);
+	}
 };
 
-export default Product;*/
+export default Search;
--- pages/products.js
+++ pages/products.js
@@ -1,11 +1,7 @@
-import useSWR from "swr";
-
 import config from "../config/config";
 import Layout from "../components/Layout";
 import Product from "../components/products/Product";
 import Search from "../components/products/Search";
-import Link from "next/link";
-import { Button, TextField } from '@material-ui/core';
 import React from 'react';
 
 class Products extends React.Component {
@@ -20,7 +16,6 @@ class Products extends React.Component {
 
 	// Define initial state
 	constructor(props) {
-		console.log("constructor");
 		super(props);
 		this.state = {
 			products: props.products,
@@ -35,88 +30,20 @@ class Products extends React.Component {
 		return (
 			<Layout>
 				<h1>Search Products</h1>
-				<TextField id="standard-basic" 
-					label="Genre" 
-					value={this.state.genreSearch}
-					onChange={event => this.setState({ genreSearch: event.target.value })}
-				/>
-				<Button variant="contained" onClick={event => this.searchByGenre()}>Search</Button>
+				<Search onSubmit={genre => this.searchByGenre(genre)} />
 				{this.state.products.map(({ product_id, name, description, price, isbn, authors, genres }) => <Product key={name} id={product_id} name={name} description={description} price={parseFloat(price)} isbn={isbn} authors={authors} genres={genres} />)}
 			</Layout>
 		);
 	}
 
-	async searchByGenre() {
-		if (!this.state.genreSearch) return;
+	async searchByGenre(genre) {
+		if (!genre) return;
 		
-		const url = "/products?genre=" + this.state.genreSearch;
+		const url = "/products?genre=" + genre;
 		const res = await fetch(config.API_GATEWAY_ENDPOINT + url);
 		const products = await res.json();
 		this.setState({ products });
 	}
 }
-
-/*
-const Products = props => {
-	console.log(products);
-	const { products } = props;
-	
-	
-	return (
-		<Layout>
-			<h1>Search Products</h1>
-			{products.map(({product_id, name, description, price, isbn, authors, genres}) => <Product key={name} id={product_id} name={name} description={description} price={parseFloat(price) }  isbn={isbn} authors={authors} genres={genres} />)}
-		</Layout>
-	);
-};
-
-Products.getInitialProps = async context => {
-	let req = config.API_GATEWAY_ENDPOINT + "/products";
-
-	const filters = [];
-    Object.keys(context.query).forEach(key => {
-        if (context.query[key]) {
-            filters.push(key + "=" + context.query[key]);
-        }
-    });
-    // Join the parts into a query
-    const filtersString = (filters.length === 0) ? "" : ("/?" + filters.join("&"));
-
-    const res = await fetch(req+filtersString);
-	console.log(res);
-	return {
-		products: await res.json()
-	};
-};*/
-
-
-
-/*const Products = props => {
-    const { products, filter } = props;
-	
-	return (
-		<Layout>
-			<h1>Search Products</h1>
-			{filter.name &&
-				<p>There was a name filter for {filter.name} but this is not supported yet.</p>
-			}
-			//{ {products.map(({id, name, price}) => <Product key={id} id={id} name={name} price={parseFloat(price)} />)} }
-			{products.map(({name, price}) => <Product key={name} id={name} name={name} price={parseFloat(price)} />)}
-		</Layout>
-	);
-};
-
-Products.getInitialProps = async context => {
-	const { name, id, minPrice, maxPrice } = context.query;
-
-    const res = await fetch(config.API_GATEWAY_ENDPOINT + "/getAllProducts");
-
-	return {
-		products: await res.json(),
-		filter: {
-			name, id, minPrice, maxPrice
-		}
-	};
-};*/
 
 export default Products;
