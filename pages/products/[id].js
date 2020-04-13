6b688961e944e9bcd6bd62e215bb057cd5236549
David Liao <liao.canada@gmail.com>
2020-04-12 22:20:20 -0400

    Make products/[id] a class instead of function

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,38 +1,39 @@
-import { useRouter } from 'next/router';
 import config from "../../config/config";
 import Layout from '../../components/Layout';
 import Link from "next/link";
 import Button from 'react-bootstrap/Button';
+import React from 'react';
 
-const Product = ({products}) => {
-	//const { isbn, name,  } = props;	
-	let product = products[0];
+class Product extends React.Component {
 
-  return (
-    <Layout>
-		<Link href="/products">
-			<Button class="btn btn-secondary btn-sm">Back to Products</Button>
-		</Link>
-    	<h1>{product.name}</h1>
-  		<p>{product.description}</p>
-		<p><i>${parseFloat(product.price).toFixed(2)}</i></p>
-		{product.isbn && <p>ISBN: {product.isbn}</p>}
-		{product.authors && <p>Author(s): {product.authors}</p>}
-		{product.genres && <p>Genre(s): {product.genres}</p>}
-		{product.series && <p>Series: {product.series}</p>}
-		
-    </Layout>
-  );
-};
-
-Product.getInitialProps = async context => {
-	const { id } = context.query;
-	// TODO get info about product with ID from DB
-	const res = await fetch(config.API_GATEWAY_ENDPOINT + "/products/"+id);
-
-	return {
-		products: await res.json()
+	static async getInitialProps(context) {
+		const { id } = context.query;
+		const res = await fetch(config.API_GATEWAY_ENDPOINT + "/products/"+id);
+	
+		return {
+			product: (await res.json())[0]
+		};
 	};
-};
+
+	render() {
+		let product = this.props.product;
+
+		return (
+		  <Layout>
+			  <Link href="/products">
+				  <Button class="btn btn-secondary btn-sm">Back to Products</Button>
+			  </Link>
+			  <h1>{product.name}</h1>
+				<p>{product.description}</p>
+			  <p><i>${parseFloat(product.price).toFixed(2)}</i></p>
+			  {product.isbn && <p>ISBN: {product.isbn}</p>}
+			  {product.authors && <p>Author(s): {product.authors}</p>}
+			  {product.genres && <p>Genre(s): {product.genres}</p>}
+			  {product.series && <p>Series: {product.series}</p>}
+			  
+		  </Layout>
+		);
+	}
+}
 
 export default Product;
