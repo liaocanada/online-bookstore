36e6edace7c08848bc67f3d95115a9a9e82a1bd8
Eric Pham <EricPham@cmail.carleton.ca>
2020-04-13 11:47:20 -0400

    Fix getAllProducts

--- lambdas/getAllProducts.js
+++ lambdas/getAllProducts.js
@@ -4,9 +4,8 @@ const formJsonResponse = require("./helpers/formJsonResponse");
 exports.handler = async (event, context) => {
     const client = await connect();
     
-    let statement = "select distinct product_id,name,description,price,isbn,series," + 
-        "format,pages,string_agg(author_name, ', ') as authors,string_agg(genre, ', ') as genres " + 
-        "from product natural full outer join ((book natural join writes) natural join book_genre) ";
+    let statement = "select distinct product_id,name,description,price,isbn,series,format,pages,authors,genres,images "+
+    "from (product natural full outer join book) natural join ";
 
     // Apply filters from query string
     const query = event.queryStringParameters || {};
@@ -33,9 +32,19 @@ exports.handler = async (event, context) => {
     // Join the parts into a query
     const filtersString = (filters.length === 0) ? "" : (" where " + filters.join(" or "));
 
-    statement += 
-        filtersString +
-        " group by product_id,name,description,price,isbn,series,format,pages;";
+    statement += "( select product_id,string_agg(genre, ', ') as genres "+
+    "from product natural full outer join (book natural join book_genre) "+filtersString+" group by product_id "+
+    ") as genre_table natural join ";
+
+    statement += "( select product_id,string_agg(author_name, ', ') as authors "+
+    "from product natural full outer join (book natural join writes) "+filtersString+" group by product_id "+
+    ") as author_table natural join ";
+
+    statement += "( select product_id,string_agg(image, ', ') as images "+
+    "from (product natural full outer join book) natural join product_image "+filtersString+" group by product_id "+
+    ") as pic_table;";
+
+    
 
     // Execute the query
     console.log(statement, values);
@@ -51,6 +60,17 @@ select distinct product_id,name,description,price,isbn,series,
         format,pages,string_agg(author_name, ', ') as authors,string_agg(genre, ', ') as genres
         from product natural full outer join ((book natural join writes) natural join book_genre) 
 		where upper(name) like upper(concat('%','harr','%'))
-		group by product_id,name,description,price,isbn,series,format,pages
+        group by product_id,name,description,price,isbn,series,format,pages
+        
+
+select product_id,string_agg(author_name, ', ') as authors
+from product natural full outer join (book natural join writes)
+where upper(name) like upper(concat('%','harr','%'))
+group by product_id
+
+select product_id,string_agg(genre, ', ') as genres
+from product natural full outer join (book natural join book_genre)
+where upper(name) like upper(concat('%','harr','%'))
+group by product_id
 		
  */ 
