599d50f9b284fda761b461027fb1c4677edceb3c
David Liao <liao.canada@gmail.com>
2020-04-13 19:08:46 -0400

    Add validation to addProductToCart lambda

--- lambdas/addProductToCart.js
+++ lambdas/addProductToCart.js
@@ -2,22 +2,30 @@ const connect = require("./helpers/connectToDatabase");
 const formJsonResponse = require("./helpers/formJsonResponse");
 
 exports.handler = async (event, context) => {
-    const client = await connect();
     const requestBody = JSON.parse(event.body);
+    const expectedKeys = ["product_id", "quantity"];
+    
+    if (!validateRequestBody(requestBody, expectedKeys)) {
+        return formTextResponse(400, 
+            "Missing request body attributes: one of " + expectedKeys.toString());
+    }
+
     const { product_id, quantity } = requestBody;
 
-    const user = event.pathParameters.username;
+    const username = event.pathParameters.username;
+
+    const client = await connect();
 
     // update user's cart last_edited
-    const update = require("./helpers/updateCart")(client,user);
+    const update = require("./helpers/updateCart")(client,username);
 
     // add product to cart_product
     const statement = "insert into cart_product (username, product_id, quantity) values ($1, $2, $3);";
-    const values = [user, product_id, quantity];
+    const values = [username, product_id, quantity];
 
     const res = await client.query(statement, values);
 
     client.end();
 
-    return formJsonResponse(201, {product_id: prod_id});
+    return formJsonResponse(201, { username, product_id, quantity });
 };
