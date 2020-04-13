347b5bfd14ccf4b88b0619bdaa631e1a3d24a24d
Eric Pham <EricPham@cmail.carleton.ca>
2020-04-13 12:12:13 -0400

    Add tag searching in getAllProducts

--- lambdas/getAllProducts.js
+++ lambdas/getAllProducts.js
@@ -4,7 +4,7 @@ const formJsonResponse = require("./helpers/formJsonResponse");
 exports.handler = async (event, context) => {
     const client = await connect();
     
-    let statement = "select distinct product_id,name,description,price,isbn,series,format,pages,authors,genres,images "+
+    let statement = "select distinct product_id,name,description,price,isbn,series,format,pages,authors,genres,tags,images "+
     "from (product natural full outer join book) natural join ";
 
     // Apply filters from query string
@@ -15,7 +15,8 @@ exports.handler = async (event, context) => {
         isbn: query.isbn,
         genre: query.genre,
         series: query.series,
-        format: query.format
+        format: query.format,
+        tag: query.tag
     };
     let counter = 1;
     const filters = [];
@@ -40,6 +41,10 @@ exports.handler = async (event, context) => {
     "from product natural full outer join (book natural join writes) "+filtersString+" group by product_id "+
     ") as author_table natural join ";
 
+    statement += "( select product_id,string_agg(tag, ', ') as tags "+
+    "from (product natural full outer join book) natural join product_tag "+filtersString+" group by product_id "+
+    ") as tag_table natural join ";
+
     statement += "( select product_id,string_agg(image, ', ') as images "+
     "from (product natural full outer join book) natural join product_image "+filtersString+" group by product_id "+
     ") as pic_table;";
@@ -56,21 +61,29 @@ exports.handler = async (event, context) => {
 };
 
 /*
-select distinct product_id,name,description,price,isbn,series,
-        format,pages,string_agg(author_name, ', ') as authors,string_agg(genre, ', ') as genres
-        from product natural full outer join ((book natural join writes) natural join book_genre) 
-		where upper(name) like upper(concat('%','harr','%'))
-        group by product_id,name,description,price,isbn,series,format,pages
-        
-
-select product_id,string_agg(author_name, ', ') as authors
-from product natural full outer join (book natural join writes)
-where upper(name) like upper(concat('%','harr','%'))
-group by product_id
-
-select product_id,string_agg(genre, ', ') as genres
-from product natural full outer join (book natural join book_genre)
-where upper(name) like upper(concat('%','harr','%'))
-group by product_id
+select distinct product_id,name,description,price,isbn,series,format,pages,authors,genres,images,tags
+from (product natural full outer join book) natural join
+		(
+			select product_id,string_agg(genre, ', ') as genres 
+			from product natural full outer join (book natural join book_genre)
+			where upper(name) like upper(concat('%','harr','%'))
+			group by product_id
+		) as genre_table natural join (
+			select product_id,string_agg(author_name, ', ') as authors
+			from product natural full outer join (book natural join writes)
+			where upper(name) like upper(concat('%','harr','%'))
+			group by product_id
+        ) as author_table natural join ( 
+            select product_id,string_agg(tag, ', ') as tags
+            from (product natural full outer join book) natural join product_tag
+            where upper(name) like upper(concat('%','harr','%'))
+            group by product_id
+        ) as tag_table natural join (
+			select product_id,string_agg(image, ', ') as images
+			from (product natural full outer join book) natural join product_image
+			where upper(name) like upper(concat('%','harr','%'))
+			group by product_id
+		) as pic_table
+		 
 		
  */ 
