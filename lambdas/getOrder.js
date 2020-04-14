baf58d15ff1d34fd7f108f8824f301256f21e0c3
Eric Pham <EricPham@cmail.carleton.ca>
2020-04-14 10:27:00 -0400

    Update getOrder

--- lambdas/getOrder.js
+++ lambdas/getOrder.js
@@ -7,7 +7,7 @@ exports.handler = async (event, context) => {
     const order_num = parseInt(event.pathParameters.order_number);
 
     // get order info
-    const statement = "select * from storeorder natural join (order_coupon natural join order_product) where order_number = $1;";
+    const statement = "select * from storeorder natural left outer join (order_product natural left outer join order_coupon) where order_number = $1;";
     const values = [order_num];
 
     const res = await client.query(statement, values);
