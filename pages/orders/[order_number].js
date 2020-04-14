46c2c185698b2226e5528db6dffdb4c08824c747
Eric Pham <EricPham@cmail.carleton.ca>
2020-04-14 10:52:06 -0400

    Change order page

--- pages/orders/[order_number].js
+++ pages/orders/[order_number].js
@@ -1,6 +1,6 @@
 import config from "../../config/config";
 import Layout from "../../components/Layout";
-import Product from "../../components/products/Product";
+import Product from "../../components/products/ProductCard";
 import React from 'react';
 import fetch from 'isomorphic-unfetch';
 
@@ -10,10 +10,7 @@ class Order extends React.Component {
 	static async getInitialProps(context) {
         const { order_number } = context.query;
 		const res = await fetch(config.API_GATEWAY_ENDPOINT + "/order/"+order_number);
-		return {
-            order_number: order_number,
-			products: await res.json()
-		};
+		return res;
 	}
 
 	// Define initial state
@@ -26,7 +23,8 @@ class Order extends React.Component {
             price += parseFloat(props.products[i].price);
         }
 		this.state = {
-            order_number: props.order_number,
+			order_number: props.order_number,
+			order_info:order_info,
             products: props.products,
             price: price
 		};
@@ -37,7 +35,12 @@ class Order extends React.Component {
 		return (
 			<Layout>
 				<h1>Order #{this.state.order_number}</h1>
-                <h3>Total: ${this.state.price.toString()}</h3>
+                <h3>Total: ${(this.state.price+parseFloat(this.state.order_info.delivery_fee)).toString()}</h3>
+				<h3>Ordered by: {this.state.order_info.username}</h3>
+				<h3>Status: {this.state.order_info.status}</h3>
+				<h3>Billed to: {this.state.order_info.billed_to}</h3>
+				<h3>Shipped to: {this.state.order_info.shipped_to}</h3>
+				<h3>Time placed: {this.state.order_info.time_placed}</h3>
 				{this.state.products.map(({ product_id, name, description, price, isbn, authors, genres, quantity }) => <Product key={name} id={product_id} name={name} description={description} price={parseFloat(price)} isbn={isbn} authors={authors} genres={genres} quantity={quantity} />)}
 			</Layout>
 		);
