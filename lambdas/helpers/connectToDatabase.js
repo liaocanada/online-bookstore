a66ea8cb7a5eb9aa1645c0ad5e127d15e98c9e81
David Liao <liao.canada@gmail.com>
2020-07-03 10:13:57 -0400

    Retrieve and decrypt DB credentials from AWS SSM instead of env variables

--- lambdas/helpers/connectToDatabase.js
+++ lambdas/helpers/connectToDatabase.js
@@ -1,18 +1,42 @@
-const { Client } = require("pg");
+const AWS = require('aws-sdk');
+const { Client } = require('pg');
+
 require('dotenv').config();
+const ssm = new AWS.SSM({region: 'us-east-1'});
 
 const connect = async () => {
-    const client = new Client({
-        host: process.env.DB_HOST,
-        port: 5432,
-        database: process.env.DB_DATABASE,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,  // Should use AWS Credentials Manager but it costs $
+    const awsSsmParams = [
+        '/bookstore/db/username',
+        '/bookstore/db/password',
+        '/bookstore/db/url',
+        '/bookstore/db/port',
+        '/bookstore/db/name'
+    ];
+    const params = {
+        Names: awsSsmParams,
+        WithDecryption: true
+    };
+    const { Parameters } = await new Promise((resolve, reject) => {
+        ssm.getParameters(params, (err, data) => {
+            if (err) reject(err);
+            resolve(data);
+        });
+    })
+
+    const getParameterValue = index => 
+        Parameters.find(param => param.Name === awsSsmParams[index]).Value;
+
+    const client = new Client({ 
+        user: getParameterValue(0),
+        password: getParameterValue(1),
+        host: getParameterValue(2),
+        port: parseInt(getParameterValue(3)),
+        database: getParameterValue(4)
     });
     
     await client.connect();
 
     return client;
-}
+};
 
 module.exports = connect;
