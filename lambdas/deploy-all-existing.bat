1796b442d1493a747a242422c990c44cc577758e
David Liao <liao.canada@gmail.com>
2020-07-04 11:20:10 -0400

    Move lambda dependency to inner package.json

--- lambdas/deploy-all-existing.bat
+++ lambdas/deploy-all-existing.bat
@@ -4,6 +4,8 @@ echo off
 echo WARNING: this will delete any index.js or index.zip you have in the current directory!
 pause
 
+set layerVersion=3
+
 setlocal EnableDelayedExpansion
 for %%f in (*.js) do if not "%%~f"=="runner.js" (
     set toDeploy=%%f
@@ -13,10 +15,9 @@ for %%f in (*.js) do if not "%%~f"=="runner.js" (
     powershell -Command "(gc %toDeploy%) -replace './helpers', '/opt/lambdas/helpers' | Out-File -encoding ASCII index.js"
     powershell "Compress-Archive index.js index.zip"
     aws lambda update-function-code --function-name %toDeploy% --zip-file fileb://index.zip
-
     del index.js
     del index.zip
 
     :: Set env variables, timeout, layer
-    aws lambda update-function-configuration --function-name "!toDeploy:~0,-3!" --environment "Variables={NODE_PATH=/opt/lambdas/node_modules}" --timeout 90 --layers "arn:aws:lambda:us-east-1:095371326078:layer:bookstore-layer-aurora:2"
+    aws lambda update-function-configuration --function-name "!toDeploy:~0,-3!" --environment "Variables={NODE_PATH=/opt/lambdas/node_modules}" --timeout 90 --layers "arn:aws:lambda:us-east-1:095371326078:layer:bookstore-layer-aurora:%layerVersion%"
 )
