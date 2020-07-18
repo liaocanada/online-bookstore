d6b9ec8961b50ef7de608edc3f7ad61ab76fbd46
David Liao <liao.canada@gmail.com>
2020-07-18 14:30:09 -0400

    Fix main search bar to programmatically redirect

--- src/views/shared/components/Search.js
+++ src/views/shared/components/Search.js
@@ -1,45 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Form, Button, InputGroup } from 'react-bootstrap';
 
-class Search extends React.Component {
+const handleSubmit = (search, history) => {
+  const query = search ? (`?q=${search}`) : '';
+  history.push(`/products${query}`);
+};
 
-  constructor(props) {
-    super(props);
-    this.state = { text: '' };
-  }
+const Search = () => {
+  const [text, setText] = useState('');
+  const history = useHistory();
 
-  render() {
-    return (
-      <InputGroup>
-        <InputGroup.Prepend>
-          <Form.Control as="select" custom>
-            <option>All Products</option>
-            <option>Books</option>
-            <option>Other Items</option>
-          </Form.Control>
-        </InputGroup.Prepend>
-        <Form.Control
-          value={this.state.text}
-          onChange={event => this.setState({ text: event.target.value })}
-        />
-        <InputGroup.Append>
-          <Button variant="outline-primary" onClick={() => this.handleSubmit()}>
-            Search
-					</Button>
-        </InputGroup.Append>
-      </InputGroup>
-
-      // <Form inline>
-
-      // </Form>
-    );
-  }
-
-  handleSubmit() {
-    const search = this.state.text;
-    const query = search ? (`?q=${search}`) : '';
-    // Router.push("/products" + query);
-  }
-}
+  return (
+    <InputGroup>
+      <InputGroup.Prepend>
+        <Form.Control as="select" custom>
+          <option>All Products</option>
+          <option>Books</option>
+          <option>Other Items</option>
+        </Form.Control>
+      </InputGroup.Prepend>
+      <Form.Control
+        value={text}
+        onChange={event => setText(event.target.value)}
+      />
+      <InputGroup.Append>
+        <Button variant="outline-primary" onClick={() => handleSubmit(text, history)}>
+          Search
+        </Button>
+      </InputGroup.Append>
+    </InputGroup>
+  );
+};
 
 export default Search;
