4a30d1bce9e2e72adcab41d5c4e1f3a7652f551b
David Liao <liao.canada@gmail.com>
2021-01-08 14:30:41 -0500

    Add additional data about each product in getOrder API

--- lambdas/getOrder.js
+++ lambdas/getOrder.js
@@ -12,8 +12,28 @@ exports.handler = async (event, context) => {
     let res = await client.query(statement, values);
     let info = res.records[0];
 
+    // TODO make sure order belongs to authenticated user
+
     // get order products
-    statement = "select * from order_product where order_number = :order_num;";
+    statement = "select distinct product_id,name,description,order_product.price,isbn,series,format,pages,authors,genres,tags,images "+
+                    "from order_product left outer join (product natural full outer join book) using (product_id) natural join ( "+
+                            "select product_id,string_agg(genre, ', ') as genres "+
+                            "from product natural full outer join (book natural join book_genre) "+
+                            "group by product_id "+
+                        ") as genre_table natural left join ( "+
+                            "select product_id,string_agg(author_name, ', ') as authors "+
+                            "from product natural full outer join (book natural join writes) "+
+                            "group by product_id "+
+                        ") as author_table natural left join ( "+
+                            "select product_id,string_agg(tag, ', ') as tags "+
+                            "from (product natural full outer join book) natural join product_tag "+
+                            "group by product_id "+
+                        ") as tag_table natural left join ( "+
+                            "select product_id,string_agg(image, ', ') as images "+
+                            "from (product natural full outer join book) natural join product_image "+
+                            "group by product_id "+
+                        ") as pic_table where order_number = :order_num;";
+
     values = { order_num };
     res = await client.query(statement, values);
     let products = res.records;
