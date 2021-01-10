3455d9ec4cddd0f792b0c4ed1f654c418e46cfa1
David Liao <liao.canada@gmail.com>
2021-01-10 11:39:04 -0500

    Redirect user to original page after login or logout via navbar

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ const configDefaults = {
   TAX_RATIO: 0.13,
   BOOK_PLACEHOLDER_IMAGE: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRWcKnEBkzIjaZL1W1U6t8essNmhTcyZFJQdDK_MtiPPmIX1GOM&usqp=CAU',
   LOGIN_URL: process.env.NODE_ENV === 'development' ?
-    'https://davidliaobookstore.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=3gv2ehl5eo84cijqql7sgquoc4&redirect_uri=http://localhost:3000/callback' :
-    'https://davidliaobookstore.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=3gv2ehl5eo84cijqql7sgquoc4&redirect_uri=https://ebookstore.davidliao.ca/callback',
+    'https://davidliaobookstore.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=3gv2ehl5eo84cijqql7sgquoc4&redirect_uri=http://localhost:3000/loginCallback' :
+    'https://davidliaobookstore.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=3gv2ehl5eo84cijqql7sgquoc4&redirect_uri=https://ebookstore.davidliao.ca/loginCallback',
   LOGOUT_URL: process.env.NODE_ENV === 'development' ?
-    'https://davidliaobookstore.auth.us-east-1.amazoncognito.com/logout?client_id=3gv2ehl5eo84cijqql7sgquoc4&logout_uri=http://localhost:3000/?message=logout_success' :
-    'https://davidliaobookstore.auth.us-east-1.amazoncognito.com/logout?client_id=3gv2ehl5eo84cijqql7sgquoc4&logout_uri=https://ebookstore.davidliao.ca/?message=logout_success',
+    'https://davidliaobookstore.auth.us-east-1.amazoncognito.com/logout?client_id=3gv2ehl5eo84cijqql7sgquoc4&logout_uri=http://localhost:3000/logoutCallback' :
+    'https://davidliaobookstore.auth.us-east-1.amazoncognito.com/logout?client_id=3gv2ehl5eo84cijqql7sgquoc4&logout_uri=https://ebookstore.davidliao.ca/logoutCallback',
   sessionStorage: {
     CURRENT_PATH_KEY: 'currentPath'
   },
